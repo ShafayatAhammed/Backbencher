5a9e8e4057bbf1353f2c5032b5cc9df8622b350a
ShafayatAhammed <mdshafayat013@gmail.com>
2024-08-27 17:54:04 +0600

    Added new route

--- Src/Routes/orders.route.js
+++ Src/Routes/orders.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { placeOrder } from "../Controllers/orders.controller.js";
+import { getMyOrders, placeOrder } from "../Controllers/orders.controller.js";
 import authenticationVerifier from "../Middlewares/authentication-verifier.middleware.js";
 
 const ordersRouter = Router();
 
 ordersRouter.route("/place-order").post(authenticationVerifier, placeOrder);
+ordersRouter.route("/get-my-orders").get(authenticationVerifier, getMyOrders);
 
 export default ordersRouter;
