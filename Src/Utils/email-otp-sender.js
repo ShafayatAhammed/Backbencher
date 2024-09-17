a867270e6ed1400a46c9025a5fa8dc872af22b78
ShafayatAhammed <mdshafayat013@gmail.com>
2024-09-17 23:27:08 +0600

    solved error throwing and removed console log

--- Src/Utils/email-otp-sender.js
+++ Src/Utils/email-otp-sender.js
@@ -11,12 +11,8 @@ const emailOtpSender = async (recipientName, recipientAddress, otpCode) => {
       subject: "Your One Time OTP Code",
       html: `<b>Hi ${recipientName}! Thanks For Stay With Martina. Below Is Your One Time Otp Code, That Will Expire In Next 5 Minutes. Enter The Otp Code In Martina. The Otp Code : ${otpCode}</b>`,
     });
-    console.log(`Otp code is : ${otpCode}`);
-
-    console.log("One time otp code has sent successfully.");
   } catch (err) {
-    console.log("One time otp code send unsuccessful!\n");
-    throw new Error(err);
+    throw err;
   }
 };
 
