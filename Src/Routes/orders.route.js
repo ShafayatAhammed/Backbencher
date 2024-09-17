63aa19240f48621cf784b12815d57fa0e536f601
ShafayatAhammed <mdshafayat013@gmail.com>
2024-09-17 23:05:16 +0600

    added new routes and more

--- Src/Routes/orders.route.js
+++ Src/Routes/orders.route.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import { getMyOrders, placeOrder } from "../Controllers/orders.controller.js";
+import {
+  getMyOrders,
+  getOrder,
+  getUserOrders,
+  placeOrder,
+} from "../Controllers/orders.controller.js";
 import authenticationVerifier from "../Middlewares/authentication-verifier.middleware.js";
 
 const ordersRouter = Router();
 
 ordersRouter.route("/place-order").post(authenticationVerifier, placeOrder);
 ordersRouter.route("/get-my-orders").get(authenticationVerifier, getMyOrders);
+ordersRouter
+  .route("/get-user-orders")
+  .get(authenticationVerifier, getUserOrders);
+ordersRouter.route("/get-order").get(authenticationVerifier, getOrder);
 
 export default ordersRouter;
